867cc52db8817431c762c12942d789d335ce7330
erikagiella <erikagiella93@gmail.com>
2026-06-18 23:30:21 +0200

    asset statici

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Static asset
+app.use(express.static('public'));
 
 //Start the server listner
 app.listen(port, () => {
