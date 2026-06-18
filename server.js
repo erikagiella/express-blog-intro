855c41deee18a23290a110dfed86337e920af323
erikagiella <erikagiella93@gmail.com>
2026-06-18 22:58:01 +0200

    server listner

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const app = express();
 const port = 3000;
 
 //Start the server listner
+app.listen(port, () => {
+    console.log(`Server listening http://localhost:${port}`)
+})
 
 //Define your routes
